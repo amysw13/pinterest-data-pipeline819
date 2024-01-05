eec6662bbfa83e44762317f1fa54fc7b279f8560
amysw13 <amysw13@gmail.com>
2024-01-05 12:30:50 +0000

    Updated DAG and databricks notebook

--- 124714cdee67_dag.py
+++ 124714cdee67_dag.py
@@ -1,35 +1,47 @@
 from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
 from datetime import datetime, timedelta
-from airflow.contrib.operators.databricks_operator import DatabricksSubmitRunOperator
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2022, 1, 1),
-    'email': ['amysw13@gmail.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+
+#Define params for Submit Run Operator
+notebook_task = {
+    'notebook_path': '/Users/amysw13@gmail.com/Reading, cleaning and querying Pinterest Data from mounted S3 bucket using Sparks',
 }
 
-with DAG('1234cdee67_dag',
-         default_args=default_args,
-         description='Trigger Databricks notebook on a schedule',
-         schedule_interval='@daily',
-         catchup=False,
-         tags=['Databricks']
-         ) as dag:
 
-    run_databricks_task = DatabricksSubmitRunOperator(
-        task_id='run_databricks_notebook',
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+
+default_args = {
+    'owner': 'Amy',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG('124714cdee67_dag',
+    # should be a datetime format
+    start_date= datetime(2024, 1, 5),
+    # check out possible intervals, should be a string #@hourly
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args,
+    description='Trigger Databricks notebook on a schedule',
+    tags=['Databricks']
+    ) as dag:
+
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
         databricks_conn_id='databricks_default',
-        new_cluster={
-            'spark_version': '7.3.x-scala2.12',
-            'node_type_id': 'Standard_D3_v2',
-            'num_workers': 1,
-        },
-        notebook_task={
-            'notebook_path': '/Users/amysw13@gmail.com/Reading, cleaning and querying Pinterest Data from mounted S3 bucket using Sparks', # Update the path to your Databricks notebook
-            }
+        existing_cluster_id='Pinterest Cluster',
+        notebook_task=notebook_task
     )
+    opr_submit_run
